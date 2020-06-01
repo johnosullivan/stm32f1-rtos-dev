c45ed2df4ca5fa1c8649060e67ed99bdff92240c
John O'Sullivan <jnosullivan@icloud.com>
2020-05-31 20:34:32 -0400

    tiny-aes

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@
 using namespace std;
 
 #include "clock.hpp"
+#include "aes/aes.hpp"
 //#include "i2c.h"
 //#include "utils.h"
 
@@ -250,6 +251,15 @@ static void sensor_i2c(void *args __attribute__((unused))) {
 	}
 }*/
 
+static void phex(uint8_t* str)
+{
+    uint8_t len = 16;
+    unsigned char i;
+    for (i = 0; i < len; ++i)
+        uart_puts(str[i]);
+    uart_puts("\n");
+}
+
 int main(void) {
 	// CPU clock is 72 MHz
 	rcc_clock_setup_in_hse_8mhz_out_72mhz();
@@ -277,8 +287,43 @@ int main(void) {
 
   xTaskCreate(demo_task1, "BLINK1", 100, NULL, configMAX_PRIORITIES - 1, NULL);
 
-	//xTaskCreate(sensor_i2c,"SHT31",100,NULL,configMAX_PRIORITIES - 1,NULL);
-	//xTaskCreate(i2c_status,"I2CSTATUS",100,NULL,configMAX_PRIORITIES-1,NULL);
+  // AES 256 Example
+   uint8_t i;
+
+   // 128bit key
+   uint8_t key[16] =        { (uint8_t) 0x2b, (uint8_t) 0x7e, (uint8_t) 0x15, (uint8_t) 0x16, (uint8_t) 0x28, (uint8_t) 0xae, (uint8_t) 0xd2, (uint8_t) 0xa6, (uint8_t) 0xab, (uint8_t) 0xf7, (uint8_t) 0x15, (uint8_t) 0x88, (uint8_t) 0x09, (uint8_t) 0xcf, (uint8_t) 0x4f, (uint8_t) 0x3c };
+   // 512bit text
+   uint8_t plain_text[64] = { (uint8_t) 0x6b, (uint8_t) 0xc1, (uint8_t) 0xbe, (uint8_t) 0xe2, (uint8_t) 0x2e, (uint8_t) 0x40, (uint8_t) 0x9f, (uint8_t) 0x96, (uint8_t) 0xe9, (uint8_t) 0x3d, (uint8_t) 0x7e, (uint8_t) 0x11, (uint8_t) 0x73, (uint8_t) 0x93, (uint8_t) 0x17, (uint8_t) 0x2a,
+                              (uint8_t) 0xae, (uint8_t) 0x2d, (uint8_t) 0x8a, (uint8_t) 0x57, (uint8_t) 0x1e, (uint8_t) 0x03, (uint8_t) 0xac, (uint8_t) 0x9c, (uint8_t) 0x9e, (uint8_t) 0xb7, (uint8_t) 0x6f, (uint8_t) 0xac, (uint8_t) 0x45, (uint8_t) 0xaf, (uint8_t) 0x8e, (uint8_t) 0x51,
+                              (uint8_t) 0x30, (uint8_t) 0xc8, (uint8_t) 0x1c, (uint8_t) 0x46, (uint8_t) 0xa3, (uint8_t) 0x5c, (uint8_t) 0xe4, (uint8_t) 0x11, (uint8_t) 0xe5, (uint8_t) 0xfb, (uint8_t) 0xc1, (uint8_t) 0x19, (uint8_t) 0x1a, (uint8_t) 0x0a, (uint8_t) 0x52, (uint8_t) 0xef,
+                              (uint8_t) 0xf6, (uint8_t) 0x9f, (uint8_t) 0x24, (uint8_t) 0x45, (uint8_t) 0xdf, (uint8_t) 0x4f, (uint8_t) 0x9b, (uint8_t) 0x17, (uint8_t) 0xad, (uint8_t) 0x2b, (uint8_t) 0x41, (uint8_t) 0x7b, (uint8_t) 0xe6, (uint8_t) 0x6c, (uint8_t) 0x37, (uint8_t) 0x10 };
+
+   uart_puts("Plain-Text:\n");
+   uart_puts("------");
+   for (i = (uint8_t) 0; i < (uint8_t) 4; ++i)
+   {
+       phex(plain_text + i * (uint8_t) 16);
+   }
+   uart_puts("\n");
+
+   /*
+   uart_puts("key:\n");
+   phex(key);
+   uart_puts("\n");
+
+   // print the resulting cipher as 4 x 16 byte strings
+   uart_puts("ciphertext:\n");
+
+   struct AES_ctx ctx;
+   AES_init_ctx(&ctx, key);
+
+   for (i = 0; i < 4; ++i)
+   {
+     AES_ECB_encrypt(&ctx, plain_text + (i * 16));
+     phex(plain_text + (i * 16));
+   }
+   uart_puts("\n");
+   */
 
 	vTaskStartScheduler();
 	for (;;);
